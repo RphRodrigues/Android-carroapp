1bca95a7e7308ab31f3d085aea4e41c0c2430f06
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-03-19 01:55:37 -0300

    Aplicativo simples usando TextView, EditText, Buttons e ImageView

---
+++ app/src/main/java/com/rstudio/carroapp/Carro.java
@@ -0,0 +1,92 @@
+package com.rstudio.carroapp;
+
+public class Carro {
+    private String name;
+    private String marca;
+    private String modelo;
+    private Double velocidade;
+
+    public Carro() {
+        this.velocidade = 0d;
+    }
+
+    public Carro(String name, String marca, String modelo) {
+        this.name = name;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidade = 0d;
+    }
+
+    public Boolean turbo() {
+        if (this.getVelocidade() <= 90.0) {
+            do {
+                this.setVelocidade(this.getVelocidade() + 10);
+            } while (this.getVelocidade() <= 90.0);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean stop() {
+        if (this.getVelocidade() > 0) {
+            do {
+                this.setVelocidade(this.getVelocidade() - 10);
+            } while (this.getVelocidade() > 0);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean speedUp() {
+        if (this.getVelocidade() < 100) {
+            this.setVelocidade(this.getVelocidade() + 10);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean brake() {
+        if (this.getVelocidade() > 0) {
+            this.setVelocidade(this.getVelocidade() - 10);
+            return true;
+        } else {
+            this.setVelocidade(0d);
+            return false;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(Double velocidade) {
+        this.velocidade = velocidade;
+    }
+}
---
+++ app/src/main/java/com/rstudio/carroapp/MainActivity.java
@@ -0,0 +1,141 @@
+package com.rstudio.carroapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Carro car = null;
+    private EditText name;
+    private EditText brand;
+    private EditText model;
+    private TextView speedometer;
+    private Button stop;
+    private Button turbo;
+    private Button criarCarro;
+    private Button speedUp;
+    private Button brake;
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.nomeCarroId);
+        brand = findViewById(R.id.marcaId);
+        model = findViewById(R.id.modeloId);
+        speedometer = findViewById(R.id.velocimetroId);
+        stop = findViewById(R.id.btnStopId);
+        turbo = findViewById(R.id.btnTurboId);
+        criarCarro = findViewById(R.id.btnCriarCarro);
+        speedUp = findViewById(R.id.btnAcelerarId);
+        brake = findViewById(R.id.btnFrearId);
+        img = findViewById(R.id.imageId);
+
+        criarCarro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (car == null) {
+                    if (!name.getText().toString().isEmpty()) {
+                        car = new Carro();
+                        car.setName(name.getText().toString());
+                        car.setMarca(brand.getText().toString());
+                        car.setModelo(model.getText().toString());
+
+                        Toast.makeText(getBaseContext(), "Car created successfully",
+                                Toast.LENGTH_LONG).show();
+
+                        speedometer.setText(R.string.speedometer);
+                        speedometer.append(" " + car.getVelocidade());
+                        speedometer.setVisibility(View.VISIBLE);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Car name is empty",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "The car " + car.getName() +
+                            " has already been created", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        turbo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (car != null) {
+                    if (car.turbo()) {
+                        speedometer.setText(R.string.speedometer);
+                        speedometer.append(" " + car.getVelocidade());
+                    } else {
+                        Toast.makeText(getBaseContext(), "The car is at full speed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getBaseContext(), "Enter the car data",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (car != null) {
+                    if (car.stop()) {
+                        speedometer.setText(R.string.speedometer);
+                        speedometer.append(" " + car.getVelocidade());
+                    } else {
+                        Toast.makeText(getBaseContext(), "The car is stopped",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getBaseContext(), "Enter the car data",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        brake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (car != null) {
+                    if (car.brake()) {
+                        speedometer.setText(R.string.speedometer);
+                        speedometer.append(" " + car.getVelocidade().toString());
+                    } else {
+                        Toast.makeText(MainActivity.this, "The car is stopped",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getBaseContext(), "Enter the car data",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        speedUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (car != null) {
+                    if (car.speedUp()) {
+                        speedometer.setText(R.string.speedometer);
+                        speedometer.append(" " + car.getVelocidade().toString());
+                    } else {
+                        Toast.makeText(getBaseContext(), "The car is at full speed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getBaseContext(), "Enter the car data",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
